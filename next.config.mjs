6f1af2f345f47180fe5e638d4e53b450699f443a
shivam <shivam.srtech@gmail.com>
2026-05-08 17:41:26 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-/** @type {import('next').NextConfig} */
-  const nextConfig = {
-    /* config options here */
-  };
-  // tailwind.config.js
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'brand': '#5145F1',        // Vibrant Indigo (Primary)
-        'brand-accent': '#0F172A', // Deep Navy (Heading text)
-        'brand-mute': '#EEF2FF',   // Soft Lavender (Highlights)
-        'brand-bg': '#F8FAFC',     // Light Slate (Background)
+        'brand': '#5145F1',         // Vibrant Indigo (Primary)
+        'brand-accent': '#0F172A',  // Deep Navy (Heading text)
+        'brand-mute': '#EEF2FF',    // Soft Lavender (Highlights)
+        'brand-bg': '#F8FAFC',      // Light Slate (Background)
       },
     },
   },
+  plugins: [],
 }
-
-export default nextConfig;
